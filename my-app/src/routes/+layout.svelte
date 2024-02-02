d169b02e4f948e4094aa3e7c22913954f78ebcbd
Hayzival <bryanblindorard@gmail.com>
2024-02-02 15:48:05 +0100

    initialisation du projet

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Fait par Bryan Blin--Dorard, Maxime Ronceray et Enrique Ortiz</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+
+	p {
+		/* mets le en orange */
+		color: #ff6600;
+	}
+</style>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	import logo_quiz from '$lib/images/quiz_logo.png';
+	import Button from '@smui/button';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="logo">
+			<picture>
+				<img src={logo_quiz} alt="logo" />
+			</picture>
+		</span>
+
+		Prêt à commencer ?
+	</h1>
+
+	<Button href="/quiz" variant="raised">Commencer</Button>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.logo {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.logo img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+	}
+</style>
